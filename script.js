ac31778274d2faeaee19c6964dc8442b6f8161d8
Knigh <abhikchakraborty900@gmail.com>
2023-02-26 19:11:52 +0530

    second commit

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ let startBtn = document.getElementById('start');
 let stopBtn = document.getElementById('stop');
 let resetBtn = document.getElementById('reset');
   
-let hour = 00;
-let minute = 00;
+
 let second = 00;
 let count = 00;
   
@@ -18,12 +17,10 @@ stopBtn.addEventListener('click', function () {
   
 resetBtn.addEventListener('click', function () {
     timer = false;
-    hour = 0;
-    minute = 0;
+    
     second = 0;
     count = 0;
-    document.getElementById('hr').innerHTML = "00";
-    document.getElementById('min').innerHTML = "00";
+    
     document.getElementById('sec').innerHTML = "00";
     document.getElementById('count').innerHTML = "00";
 });
@@ -37,29 +34,13 @@ function stopWatch() {
             count = 0;
         }
   
-        if (second == 60) {
-            minute++;
-            second = 0;
-        }
+        
   
-        if (minute == 60) {
-            hour++;
-            minute = 0;
-            second = 0;
-        }
-  
-        let hrString = hour;
-        let minString = minute;
+        
         let secString = second;
         let countString = count;
   
-        if (hour < 10) {
-            hrString = "0" + hrString;
-        }
-  
-        if (minute < 10) {
-            minString = "0" + minString;
-        }
+        
   
         if (second < 10) {
             secString = "0" + secString;
@@ -69,8 +50,7 @@ function stopWatch() {
             countString = "0" + countString;
         }
   
-        document.getElementById('hr').innerHTML = hrString;
-        document.getElementById('min').innerHTML = minString;
+        
         document.getElementById('sec').innerHTML = secString;
         document.getElementById('count').innerHTML = countString;
         setTimeout(stopWatch, 10);
